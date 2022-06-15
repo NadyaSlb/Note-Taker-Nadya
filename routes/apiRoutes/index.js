75905b787b144ec9d7a09e91347e8a9c9ecf3b59
Nadya <hoperu1986@gmail.com>
2022-06-15 16:40:43 -0400

    routes1

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
-const notesRoutes = require('../apiRoutes/notesRoutes');
+// const { getNotes, saveNote, deleteNote } = require('../../public/assets/js/index');
+const notes = require('../../db/db.json');
+const fs = require('fs');
 
-router.use(notesRoutes);
+router.get('/notes', (req, res) => {
+    let notes= fs.readFile("../../db/db.json",
+    )
+    res.json(notes)
+});
+
+// router.post('/notes', (req, res) => {
+  fs.appendFile
+// });
 
 module.exports = router;
